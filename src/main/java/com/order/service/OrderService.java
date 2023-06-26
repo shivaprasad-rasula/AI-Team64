69a0cee2dcf69e1949b4faa35fa6a98e4367d86e
Shiva Prasad Rasula <shiva_rasula@persistent.com>
2023-06-26 13:50:37 +0530

    code merging

---
+++ src/main/java/com/order/service/OrderService.java
@@ -0,0 +1,8 @@
+package com.order.service;
+
+import com.order.entity.Order;
+
+public interface OrderService {
+
+	public Order saveOrder(Order order);
+}
---
+++ src/main/java/com/order/util/TransactionResponse.java
@@ -0,0 +1,6 @@
+package com.order.util;
+
+public class TransactionResponse {
+
+	
+}
